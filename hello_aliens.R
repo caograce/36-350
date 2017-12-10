449bc096aa20e5922c8266cd2abe3b4683d1e162
Grace Cao <grace@Graces-MacBook-Pro.local>
2017-12-10 17:16:08 -0500

    hello_aliens.R for final project question #20

---
+++ hello_aliens.R
@@ -0,0 +1,9 @@
+cat("Hello Aliens. \n
+I wonder how life on your planet is like, and hopefully we will be able to meet one day.\n
+Although there are many problems with life on Earth, in general it is pretty good. \n
+If you ever come visit, make sure you see the fabulous creatures known as dogs. They are \n
+definitely friendlier and cuter than most human beings. Also make sure to tour all the countries \n
+and cultures around the world. What's life on your planet like? Is it also divided into many countries? \n
+There are a lot of humans (7.5 billion in fact). How many people are there on your planet? 
+Enjoy your trip on Earth if you ever come! :) \n")
+
